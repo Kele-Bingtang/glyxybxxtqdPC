c600d6c34be5a087f2f7ed18d29025f198381970
hf511 <577690236@qq.com>
2021-04-22 22:31:25 +0800

    修改报修区域

--- src/views/pc/index/index.vue
+++ src/views/pc/index/index.vue
@@ -185,9 +185,30 @@
     },
     mounted() {
       this.getDatas()
-      this.autoMonitor()
+      this.autoMonitor();
+      this.autoFullScreen();
     },
     methods: {
+      //自动全屏
+      autoFullScreen(){
+        console.log("-----------")
+        if(document.documentElement.RequestFullScreen){
+          document.documentElement.RequestFullScreen();
+        }
+        //兼容火狐
+        console.log(document.documentElement.mozRequestFullScreen)
+        if(document.documentElement.mozRequestFullScreen){
+          document.documentElement.mozRequestFullScreen();
+        }
+        //兼容谷歌等可以webkitRequestFullScreen也可以webkitRequestFullscreen
+        if(document.documentElement.webkitRequestFullScreen){
+          document.documentElement.webkitRequestFullScreen();
+        }
+        //兼容IE,只能写msRequestFullscreen
+        if(document.documentElement.msRequestFullscreen){
+          document.documentElement.msRequestFullscreen();
+        }
+      },
       getDatas() {
         AdminServlet({
           op: 'adminindex'
